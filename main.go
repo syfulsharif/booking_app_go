10f8e6662ade7d39cc0ab98e149fbc7859e7964f
Syful Sharif <sharif.syful@gmail.com>
2023-02-07 19:49:29 +0600

    total tickets const and remainingtickets variable added

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var conferenceName = "Go Conferenece"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+	fmt.Println("Welcome to", conferenceName, "booking application.")
+	fmt.Println("We have total of", conferenceTickets, "tickets and currently", remainingTickets, "tickets left.")
+	fmt.Println("Get your tickets here to attend.")
+
 }
