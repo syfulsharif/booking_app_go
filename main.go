49440109a8b3efc382e3f89cd408bbe20691db15
Syful Sharif <sharif.syful@gmail.com>
2023-02-17 19:47:57 +0600

    Printing format updated

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ func main() {
 	var conferenceName = "Go Conferenece"
 	const conferenceTickets = 50
 	var remainingTickets = 50
-	fmt.Println("Welcome to", conferenceName, "booking application.")
-	fmt.Println("We have total of", conferenceTickets, "tickets and currently", remainingTickets, "tickets left.")
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and currently %v tickets left.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
-
 }
