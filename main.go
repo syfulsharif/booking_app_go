5da46dd29bf7ef27ebcd4c92a48c7e3666b48d3a
Syful Sharif <sharif.syful@gmail.com>
2023-03-04 09:18:43 +0600

    Validation added

--- main.go
+++ main.go
@@ -38,7 +38,11 @@ func main() {
 		fmt.Println("Enter the number of tickets you want to book: ")
 		fmt.Scan(&userTickets)
 
-		if userTickets < remainingTickets {
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets < remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, firstName+" "+lastName)
 
@@ -64,7 +68,7 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Printf("We only have %v tickets left, so you can't book %v tickets.\n", remainingTickets, userTickets)
+			fmt.Println("You have entered invalid input")
 			continue
 		}
 
