3a2cba2fbce4b00f288d844d96f4ecd331a0be26
Syful Sharif <sharif.syful@gmail.com>
2023-03-26 19:23:16 +0600

    Package Added

--- main.go
+++ main.go
@@ -69,13 +69,6 @@ func getFirstNames() []string {
 	// fmt.Printf("These are first names of all our bookings are: %v\n", firstNames)
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets < remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getUserInput() (string, string, string, uint) {
 
 	var firstName string
